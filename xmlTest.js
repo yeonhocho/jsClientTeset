e92dced217a353393346af6d84a75cdd470e6c7d
조연호 <37644200+yeonhocho@users.noreply.github.com>
2023-05-11 15:44:07 +0900

    Update xmlTest.js

--- xmlTest.js
+++ xmlTest.js
@@ -1,11 +1,19 @@
 const xhr = new XMLHttpRequest();
 const url = "http://localhost:50002/";
 
+let jsonData;
+
 console.log("start client " + url);
 xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200){
         const response = xhr.responseText;
-        console.log(response);
+        jsonData = JSON.parse(response);
+        let text = document.querySelector(".rank")
+        for(var i = 0; i < Object.keys(jsonData).length;i++)
+        {
+            text.innerHTML +="<p>"+jsonData[i]["name"]+","
+            + jsonData[i]["value"]+"</p></br>";
+        }
     }
 
 };
